e938ce5c8f7c32a180f8bdb745c15bc1adb9f28f
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-10-11 14:54:33 +0300

    Add auth

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -8,7 +8,6 @@ use Illuminate\Http\Request;
 class PublicController extends Controller
 {
     public function index(){
-
         $posts = Post::simplePaginate(16);
         return view('welcome', compact('posts'));
     }
