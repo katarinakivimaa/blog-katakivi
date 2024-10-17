f794dbdb29db69b542cdc8c01a4259ebd8a95134
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-10-17 11:14:24 +0300

    Fix auth to use daisyui

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,10 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/secure', function () {
+    return view('secure');
+})->middleware(['auth', 'verified', 'password.confirm'])->name('secure');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
