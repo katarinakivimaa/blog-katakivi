5a77c57105a4192b93917f6f6950c2178ae08062
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-10-18 14:48:40 +0300

    Add posts.index create

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
     <div class="grid-cols-4 grid gap-4">
         @foreach($posts as $post)
             <div>
-                <div class="card bg-base-100 shadow-xl">
+                <div class="card bg-base-100 shadow-xl min-h-full">
                     <figure>
                         <img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
                             alt="Shoes" />
@@ -15,7 +15,7 @@
                         <h2 class="card-title">{{ $post->title }}</h2>
                         <p>{{ $post->snippet }}</p>
                         <div class="card-actions justify-end">
-                            <button class="btn btn-primary">Buy Now</button>
+                            <a href="{{route('post', ['post'=>$post])}}" class="btn btn-primary">Read More</a>
                         </div>
                     </div>
                 </div>
